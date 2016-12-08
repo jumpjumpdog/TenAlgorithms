d9ef6fbc14f9e52954f79d2b48c597e57b04502e
JonathanKingYo <838455960@qq.com>
2016-12-08 19:30:24 +0800

    jonathan

---
+++ Jonathan.cpp
@@ -0,0 +1,31 @@
+#include"iostream"
+
+using std::cin;
+using std::cout;
+using std::endl;
+
+
+int main() {
+	cout << "**               排序算法比较             **" << endl;
+	cout << "============================================" << endl;
+	cout << "**               1--冒泡排序              **" << endl;
+	cout << "**               2--选择排序              **" << endl;
+	cout << "**               3--直接插入排序          **" << endl;
+	cout << "**               4--希尔排序              **" << endl;
+	cout << "**               5--快速排序              **" << endl;
+	cout << "**               6--堆排序                **" << endl;
+	cout << "**               7--归并排序              **" << endl;
+	cout << "**               8--基数排序              **" << endl;
+	cout << "**               9--折半插入排序          **" << endl;
+	cout << "**               10--锦标赛排序           **" << endl;
+	cout << "============================================" << endl;
+
+
+	int choice,num;
+	cout << "请输入要产生的随机数的个数:";
+	cin >> num;
+
+	while (true) {
+		cin >> choice;
+	}
+}
